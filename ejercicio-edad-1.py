9d29121ca7773e627f3433641f1aa985da7af136
Carlos Moreno Casquero <CaMoCa21@localhost.localdomain>
2017-03-02 15:38:19 +0100

    2-3-2017

---
+++ ejercicio-edad-1.py
@@ -0,0 +1,8 @@
+#! /usr/bin/python
+# coding: utf8
+
+edad= input ("Indique su edad: ")
+if edad >= 15 and edad <= 17:
+	print "puedes pasar a la sesion de tarde"
+		
+ 
---
+++ ejercicio-edad-2.py
@@ -0,0 +1,13 @@
+#! /usr/bin/python
+# coding: utf8
+
+edad= input ("Indique su edad: ")
+if (edad >= 18 and edad <= 23) :
+	print "Hola"
+	print "Eres joven, puedes pasar a la sesion de jóvenes"
+	
+else:
+	print "puedes entrar en la sesion de jovenes"
+	
+		
+ 
