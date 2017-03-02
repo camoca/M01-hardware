6387a067b482a2f44b0e8a51db670901420417b4
Carlos Moreno Casquero <CaMoCa21@localhost.localdomain>
2017-03-02 17:09:12 +0100

    bien hecho

--- ejercicio-edad-2.py
+++ ejercicio-edad-2.py
@@ -2,12 +2,11 @@
 # coding: utf8
 
 edad= input ("Indique su edad: ")
-if (edad >= 18 and edad <= 23) :
+if (edad >= 18 and edad <= 23) or edad == 17 :
 	print "Hola"
 	print "Eres joven, puedes pasar a la sesion de jóvenes"
 	
-else:
-	print "puedes entrar en la sesion de jovenes"
+
 	
 		
  
