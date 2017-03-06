d3ec918d1069b248b467e3a4f72b0c265483eab4
root <root@a17.informatica.escoladeltreball.org>
2017-03-06 13:03:57 +0100

    hola

---
+++ M03/03-03-2017/ejercicio-edad-3.py
@@ -0,0 +1,10 @@
+#! /usr/bin/python
+# coding: utf8
+
+
+
+edad= input ("Indique su edad: ")
+
+if (edad >= 18 and edad <= 23) or edad == 17:
+	print "Hola"
+	print "Eres joven, puedes pasar a la sesion de jóvenes"
