d7b83b918e28af0a92ec8889c6bb1f951e7f0a00
root <root@a17.informatica.escoladeltreball.org>
2017-03-06 12:52:48 +0100

    probar al llegar a casa

--- M03/ejercicos-edad-3_par_condicion-chunga/ejercicio-edad-3.py
+++ M03/ejercicos-edad-3_par_condicion-chunga/ejercicio-edad-3.py
@@ -8,6 +8,3 @@ edad= input ("Indique su edad: ")
 if (edad >= 18 and edad <= 23) or edad == 17:
 	print "Hola"
 	print "Eres joven, puedes pasar a la sesion de jóvenes"
-	
-		
- 
---
+++ M03/ejercicos-edad-3_par_condicion-chunga/ejercicio-pares.py
@@ -0,0 +1,15 @@
+#! /usr/bin/python
+# coding: utf8
+
+numero= input ("Introduce un numero: ")
+
+if numero % 2 == 0:
+	print "Qué bonito número par"
+else: 
+	print "Qué número ḿas vulgar"
+
+
+
+
+
+
