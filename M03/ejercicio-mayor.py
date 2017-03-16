07ac931a6ffb012ee3845b953142a0d581533469
Carlos Moreno Casquero <CaMoCa21@localhost.localdomain>
2017-03-16 19:38:10 +0100

    ejercicos joni

---
+++ M03/ejercicio-mayor.py
@@ -0,0 +1,14 @@
+#coding: utf8
+import os
+os.system('clear')
+
+valor1 = input("Escriba un numero: ")
+valor2 = input("Escriba otro numero: ")
+
+if valor1 > valor2:
+	print "Este numero" , valor1 , "es mas grande que el numero" , valor2
+else:
+	if valor1 == valor2:
+		print "Estos numeros son iguales"
+	else:
+		print "El numero" , valor2 , "es mas grande que el numero" , valor1
---
+++ M03/ejercicio-swap.py
@@ -0,0 +1,15 @@
+#coding: utf8
+
+import os
+
+os.system('clear')
+
+mano_derecha = "móvil"
+mano_izquierda = "bocadillo"
+
+print "El movil esta en la mano Derecha."
+print "El bocadillo esta en la mano Izquierda."
+
+mano_extra = mano_derecha
+mano_derecha = mano_izquierda
+mano_izquierda = mano_extra
