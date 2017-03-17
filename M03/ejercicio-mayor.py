6108b8f9cd9bc5f79aeb14f250737439d2a7f93c
Carlos Moreno Casquero <carlos.moreno2000@hotmail.com>
2017-03-17 08:15:35 +0100

    Update ejercicio-mayor.py

--- M03/ejercicio-mayor.py
+++ M03/ejercicio-mayor.py
@@ -6,7 +6,7 @@ valor1 = input("Escriba un numero: ")
 valor2 = input("Escriba otro numero: ")
 
 if valor1 > valor2:
-	print "Este numero" , valor1 , "es mas grande que el numero" , valor2
+	print "Este numero" , valor1 , "es mas grande que " , valor2
 else:
 	if valor1 == valor2:
 		print "Estos numeros son iguales"
