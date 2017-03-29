472c1d1d08346461c13b85adb7b5f94458527fd6
ism25366309 <ism25366309@a17.informatica.escoladeltreball.org>
2017-03-29 12:53:55 +0200

    RAID

---
+++ M03/bucles_fets_a_clase/untitled.py
@@ -0,0 +1,17 @@
+#coding: utf8
+
+num = 1
+contador = 1
+sortir = False
+
+while (sortir == False):
+	
+	print num
+	
+	if (num %num==2):
+	
+		sortir = True
+	
+	contador = contador / num
+	num = num + 1
+print contador    
