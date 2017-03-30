b4f08046fe45f55f8b9bf0b7e40278ff58abd63b
ism25366309 <ism25366309@a17.informatica.escoladeltreball.org>
2017-03-30 13:21:54 +0200

    piedra_papel_tijera

---
+++ M03/piedra_papel_tijera.py
@@ -0,0 +1,34 @@
+#coding: utf8
+
+j1 = raw_input ('J1: piedra, papel o tijera ')
+j2 = raw_input ('J2: piedra, papel o tijera ')
+
+if (j1 == "piedra") and (j2 == "piedra"):
+	print "Hay un EMPATE"
+		
+if (j1 == "piedra") and (j2 == "papel"):
+	print "Gana J2"
+		
+if (j1 == "piedra") and (j2 == "tijera"):
+	print "Gana J1"
+		
+if (j1 == "papel") and (j2 == "piedra"):
+	print "Gana J1"
+		
+if (j1 == "papel") and (j2 == "papel"):
+	print "Hay un EMPATE"
+		
+if (j1 == "papel") and (j2 == "tijera"):
+	print "Gana J2"
+		
+if (j1 == "tijera") and (j2 == "piedra"):
+	print "Gana J2"
+		
+if (j1 == "tijera") and (j2 == "papel"):
+	print "Gana J1"
+
+if (j1 == "tijera") and (j2 == "tijera"):
+	print "Hay un EMPATE"
+		
+	
+	
