7fe7748a90359391d4d68cae839de9d626150125
Carlos Moreno Casquero <CaMoCa21@localhost.localdomain>
2017-04-05 17:11:51 +0200

    POKER

---
+++ M03/POKER/POKER.py
@@ -0,0 +1,68 @@
+#coding: utf8
+
+from random import randint
+salir =False
+while (salir== False):
+	palo= randint (1,4)
+
+	if (palo == 1):
+		palos = "PICAS"
+
+	if (palo == 2):
+		palos = "ROMBOS"
+		
+	if (palo == 3):
+		palos = "TREBOL"
+		
+	if (palo == 4):
+		palos = "CORAZONES"
+		
+	palo= randint (1,4)
+
+	if (palo == 1):
+		paloss = "PICAS"
+
+	if (palo == 2):
+		paloss = "ROMBOS"
+		
+	if (palo == 3):
+		paloss = "TREBOL"
+		
+	if (palo == 4):
+		paloss = "CORAZONES"
+		
+
+	J1= randint (1,13)
+	J2= randint (1,13)
+
+	if (J1 == 11):
+		J1 = "J"
+		
+	if (J1 == 12):
+		J1 = "Q"
+		
+	if (J1 == 13):
+		J1 = "K"
+		
+	if (J2 == 11):
+		J2 = "J"
+		
+	if (J2 == 12):
+		J2 = "Q"
+		
+	if (J2 == 13):
+		J2 = "K"
+		
+	print "J1 tiene", J1, "de", palos
+	print "J2 tiene", J2, "de", paloss
+
+	if (J1 == J2):
+		print "Hay un empate"
+		
+	if (J1 < J2):
+		print "GANA J1"
+	else:
+		print "GANA J2"
+	
+	if (J1 <> J2):
+		salir = True
