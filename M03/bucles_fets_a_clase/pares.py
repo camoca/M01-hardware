372a6b56b5deb82c0c34f55d9a5a3aeecc03902f
Carlos Moreno Casquero <carlos.moreno2000@hotmail.com>
2017-05-15 18:39:44 +0200

    Update pares.py

--- M03/bucles_fets_a_clase/pares.py
+++ M03/bucles_fets_a_clase/pares.py
@@ -1,7 +1,17 @@
 #coding: utf8
+
+##########################
+#  ASIGNAMOS VARIABLES   #
+##########################
+
 numero = 1
 final = input ('Introduzca un numero: ')
 sortir = False 
+
+######################################
+#  GENERAMOS BUCLE CON CONDICIONES   #
+######################################
+
 while sortir == False:
 	if (numero %2 == 0):
 		print numero
