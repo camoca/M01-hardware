241a6fd21449602ebbeecd389a8b355271adbbb7
Carlos Moreno Casquero <carlos.moreno2000@hotmail.com>
2017-05-15 17:52:39 +0200

    Update ejercicio-pares.py

--- M03/07-03-2017/ejercicio-pares.py
+++ M03/07-03-2017/ejercicio-pares.py
@@ -1,7 +1,17 @@
 #! /usr/bin/python
 # coding: utf8
 
+###################################
+#  PEDIMOS QUE INDIQUE UN NUMERO  #
+###################################
+
+
 numero= input ("Introduce un numero: ")
+
+
+###################################
+#  PONEMOS CONDICION Y PRINTAMOS  #
+###################################
 
 if numero % 2 == 0:
 	print "Qué bonito número par"
