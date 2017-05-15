3ef315cab96c4f55bd3a4fdef8703add4c67c13f
Carlos Moreno Casquero <carlos.moreno2000@hotmail.com>
2017-05-15 17:26:34 +0200

    Update ejercicio-edad-1.py

--- M03/03-03-2017/ejercicio-edad-1.py
+++ M03/03-03-2017/ejercicio-edad-1.py
@@ -1,7 +1,16 @@
 #! /usr/bin/python
 # coding: utf8
 
+#################################
+#  PEDIMOS QUE INDIQUE LA EDAD  #
+#################################
+
 edad= input ("Indique su edad: ")
+
+###################################
+#  PONEMOS CONDICION Y PRINTAMOS  #
+###################################
+
 if edad >= 15 and edad <= 17:
 	print "puedes pasar a la sesion de tarde"
 		
